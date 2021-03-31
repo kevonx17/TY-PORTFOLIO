f46b0af8857345095c6a9e3ba6525d78c125abb3
Tyrone Arthurs <kevonx15@gmail.com>
2021-03-30 23:36:12 -0500

    Added the frontend models

--- ng-fb/src/environments/environment.dev.ts
+++ ng-fb/src/environments/environment.dev.ts
@@ -3,7 +3,12 @@ export const environment = {
   name: 'dev',
   firebase: {
     config: {
-     
+      apiKey: 'AIzaSyD1lp8C3oO2FB2T81xQE7HGIWQU1P15jsc',
+      authDomain: 'ty-portfolio.firebaseapp.com',
+      projectId: 'ty-portfolio',
+      storageBucket: 'ty-portfolio.appspot.com',
+      messagingSenderId: '161985163688',
+      appId: '1:161985163688:web:52c4e2ac96dd10bcfeed9d'
     },
   },
 };
