808cbefe7f452ff885c15cffd92ff38cedabc5f8
Tyrone Arthurs <kevonx15@gmail.com>
2021-03-30 22:49:33 -0500

    Added form-field with validations

--- ng-fb/src/app/pages/demo/pages/shared/shared.component.ts
+++ ng-fb/src/app/pages/demo/pages/shared/shared.component.ts
@@ -4,31 +4,38 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
-  styleUrls: ['./shared.component.scss']
+  styleUrls: ['./shared.component.scss'],
 })
 export class SharedComponent implements OnInit {
-
   form: FormGroup;
+  isInline: boolean;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      input: [null]
-    })
+      input: [null, {
+        updateOn: 'blur',
+        validators: [
+            Validators.required,
+            Validators.minLength(3)
+            //Validators.pattern(regex.numbers)
+        ]
+    }]
+    });
   }
 
-onSubmit(): void {
-        console.log('Submit!');
+  onSubmit(): void {
+    console.log('Submit!');
 
-        // if (!this.form.valid) {
-        //     markFormGroupTouched(this.form);
-        // }
-    }
+    // if (!this.form.valid) {
+    //     markFormGroupTouched(this.form);
+    // }
+  }
 
   onPatchValue(): void {
-    this.form.patchValue({input: 'test'});
-       /* input: 123,
+    this.form.patchValue({ input: 'test' });
+    /* input: 123,
         password: 'qwerty',
         autocomplete: 1,
         select: 2,
@@ -40,6 +47,7 @@ onSubmit(): void {
             to: new Date(2019, 5, 25).getTime()
         }
     });*/
-}
+  }
 
+  onToggleInline(): void { this.isInline = !this.isInline;  }
 }
